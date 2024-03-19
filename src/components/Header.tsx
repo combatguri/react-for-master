17f59ba45fd0fdb15255b88d22439c411dcab1d6
combatguri <combatguri92@gmail.com>
2024-03-20 02:26:53 +0900

    logo

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,28 +2,38 @@ import React from "react";
 import { Link, useNavigate } from "react-router-dom";
 import styled from "styled-components";
 
+import logo from "../assets/logo.svg";
+
 const HeaderContainer = styled.header`
+  display: flex;
+  justify-content: center;
+  align-items: center;
   margin-bottom: 1em;
+  padding: 1em;
 `;
 
-const Ul = styled.ul`
-  display: flex;
-  padding: 1em;
-  align-items: center;
-  justify-content: center;
-  li {
-    font-weight: bold;
-    padding: 0 0.5em;
+const LogoWrapper = styled.div`
+  img {
+    height: 30px;
+  }
+`;
+
+const MenuWrapper = styled.div`
+  ul {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    margin-bottom: 3px;
+    li {
+      font-weight: bold;
+      padding: 0 0.5em;
+    }
   }
 `;
 
 const BtnSpan = styled.span`
   cursor: pointer;
   text-decoration: underline;
-`;
-
-const Img = styled.img`
-  width: 50vw;
 `;
 
 function Header() {
@@ -34,20 +44,25 @@ function Header() {
 
   return (
     <HeaderContainer>
-      <Ul>
-        <li>
-          <Link to="/">HOME</Link>
-        </li>
-        <li>
-          <Link to="/coin">COIN</Link>
-        </li>
-        <li>
-          <BtnSpan onClick={goToAbout}>ABOUT</BtnSpan>
-        </li>
-        <li>
-          <Link to="/users">Users</Link>
-        </li>
-      </Ul>
+      <LogoWrapper>
+        <img src={logo} className="App-logo" alt="logo" />
+      </LogoWrapper>
+      <MenuWrapper>
+        <ul>
+          <li>
+            <Link to="/">HOME</Link>
+          </li>
+          <li>
+            <Link to="/coin">COIN</Link>
+          </li>
+          <li>
+            <BtnSpan onClick={goToAbout}>ABOUT</BtnSpan>
+          </li>
+          <li>
+            <Link to="/users">Users</Link>
+          </li>
+        </ul>
+      </MenuWrapper>
     </HeaderContainer>
   );
 }
