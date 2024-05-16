a2c06e3a4bcab3d6272afd6b5ea3f4c48e2213fc
ChadJeong <combatguri92@gmail.com>
2024-05-16 17:42:42 +0900

    fix:

--- src/api/apis.ts
+++ src/api/apis.ts
@@ -1,22 +1,31 @@
-export function setItemWithExpiry(key: string, value: string | object, ttl: number) {
+const expiredSessionTime = 1000 * 60 * 10; // 10분
+
+export function setItemWithExpiry(key: string, value: string | object, ttl: number = expiredSessionTime) {
+    console.log(`setItemWithExpiry:`, {
+        key,
+        value,
+        ttl
+    });
     const now = new Date();
     const item = {
         value: value,
         expiry: now.getTime() + ttl
     };
+    console.log(111, item);
     sessionStorage.setItem(key, JSON.stringify(item));
 }
 
 export function getItemWithExpiry(key: string) {
-    const itemStr = sessionStorage.getItem(key);
-
+    const itemStr = sessionStorage.getItem(key) as string | null;
+    console.log(`getItemWithExpiry:`, {
+        key,
+        itemStr
+    });
     if (!itemStr) {
         return null;
     }
-
     const item = JSON.parse(itemStr);
     const now = new Date();
-
     if (now.getTime() > item.expiry) {
         sessionStorage.removeItem(key);
         return null;
@@ -29,9 +38,10 @@ export function clearSessionStorage(): void {
 }
 
 export function setSessionStorage(key: string, value: string | object): void {
-    window.sessionStorage.setItem(`${key}`, typeof value !== 'string' ? JSON.stringify(value) : value);
+    setItemWithExpiry(key, value);
+    // window.sessionStorage.setItem(`${key}`, typeof value !== 'string' ? JSON.stringify(value) : value);
 }
 
 export function getSessionStorage(key: string) {
-    return JSON.parse(window.sessionStorage.getItem(`${key}`) as string);
+    return JSON.parse(getItemWithExpiry(key));
 }
