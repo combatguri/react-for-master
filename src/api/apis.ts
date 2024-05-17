8d8476dd6c85e7de9d683367f2939a99a79f0493
ChadJeong <combatguri92@gmail.com>
2024-05-17 13:45:43 +0900

    user storage, need refactoring

--- src/api/apis.ts
+++ src/api/apis.ts
@@ -1,47 +1,96 @@
-const expiredSessionTime = 1000 * 60 * 10; // 10분
+const expiredStorageTime = 1000 * 60 * 1; // 10분
 
-export function setItemWithExpiry(key: string, value: string | object, ttl: number = expiredSessionTime) {
-    console.log(`setItemWithExpiry:`, {
-        key,
-        value,
-        ttl
-    });
-    const now = new Date();
-    const item = {
-        value: value,
-        expiry: now.getTime() + ttl
-    };
-    console.log(111, item);
-    sessionStorage.setItem(key, JSON.stringify(item));
+// 스토리지 interface
+interface IStorage {
+    value: string; // JSON.stringify(value)
+    expiry: number; // 만료 시간
 }
 
-export function getItemWithExpiry(key: string) {
-    const itemStr = sessionStorage.getItem(key) as string | null;
-    console.log(`getItemWithExpiry:`, {
+/**
+ * IStorage 타입으로 스토리지에 저장한다.
+ * @param key           스토리지 key
+ * @param value         스토리지 value
+ * @param ttl           스토리지 만료시간
+ * @param isSession     세션스토리지:true, 로컬스토리지:false(default)
+ */
+function setStorageItemWithExpire(key: string, value: string | object, ttl: number = expiredStorageTime, isSession: boolean = false) {
+    const now = new Date();
+    const item: IStorage = {
+        value: typeof value === 'string' ? value : JSON.stringify(value),
+        expiry: now.getTime() + ttl
+    };
+    const storage = isSession ? sessionStorage : localStorage;
+    storage.setItem(key, JSON.stringify(item));
+}
+
+// 하위 item 으로 넣으며 expired 걸기
+export function setStorageInnerItemWithExpire(key: string, childKey: string, value: string | object, ttl: number = expiredStorageTime, isSession: boolean = false) {
+    const storage = isSession ? sessionStorage : localStorage;
+    const itemStr = storage.getItem(key) as string | null;
+    const oldItem = itemStr ? JSON.parse(itemStr) : {};
+    const now = new Date();
+    storage.setItem(
         key,
-        itemStr
-    });
+        JSON.stringify({
+            ...oldItem,
+            [childKey]: {
+                value: typeof value === 'string' ? value : JSON.stringify(value),
+                expiry: now.getTime() + ttl
+            } as IStorage
+        })
+    );
+}
+
+// 하위 item 으로 검색할때 expired 제거
+export function getStorageInnerItemWithExpire(key: string, childKey: string, isSession: boolean = false) {
+    const storage = isSession ? sessionStorage : localStorage;
+    const itemStr = storage.getItem(key) as string | null;
+    if (!itemStr) {
+        return null;
+    }
+    const oldItem = JSON.parse(itemStr);
+    const childItem = oldItem[childKey];
+    if (!childItem) {
+        return null;
+    }
+    const now = new Date();
+    if (now.getTime() > childItem.expiry) {
+        delete oldItem[childKey];
+        storage.setItem(key, JSON.stringify(oldItem));
+        return null;
+    }
+    return JSON.parse(childItem.value);
+}
+
+/**
+ * 스토리지 값 get
+ * @param key           스토리지 key
+ * @param isSession     세션스토리지:true, 로컬스토리지:false(default)
+ */
+function getStorageItemWithExpire(key: string, isSession: boolean = false) {
+    const storage = isSession ? sessionStorage : localStorage;
+    const itemStr = storage.getItem(key) as string | null;
     if (!itemStr) {
         return null;
     }
     const item = JSON.parse(itemStr);
     const now = new Date();
     if (now.getTime() > item.expiry) {
-        sessionStorage.removeItem(key);
+        storage.removeItem(key);
         return null;
     }
     return item.value;
 }
 
-export function clearSessionStorage(): void {
-    sessionStorage.clear();
+export function clearStorage(isSession: boolean = false): void {
+    const storage = isSession ? sessionStorage : localStorage;
+    storage.clear();
 }
 
 export function setSessionStorage(key: string, value: string | object): void {
-    setItemWithExpiry(key, value);
-    // window.sessionStorage.setItem(`${key}`, typeof value !== 'string' ? JSON.stringify(value) : value);
+    setStorageItemWithExpire(key, value);
 }
 
 export function getSessionStorage(key: string) {
-    return JSON.parse(getItemWithExpiry(key));
+    return JSON.parse(getStorageItemWithExpire(key));
 }
--- src/api/coins.ts
+++ src/api/coins.ts
@@ -1,7 +1,10 @@
 import * as APIS from './apis';
 import { ICoin, ICoins } from '../screens/coin/Interface';
+import { rejects } from 'node:assert';
+import error from '../screens/Error';
+import { setStorageInnerItemWithExpire } from './apis';
 
-const options = {
+const fetchOptions = {
     method: 'GET',
     headers: {
         'x-cg-demo-api-key': 'CG-TE3QzKqHSAsfG3f9FjKXCc6N'
@@ -17,32 +20,32 @@ const BASE_URL = {
     coingecko: `https://api.coingecko.com/api/v3` //https://docs.coingecko.com/v3.0.1/reference/coins-markets
 };
 
+// export async function fetchCoinList2() {
+//     const currency = 'usd';
+//     const perPage = 20;
+//     const nowPage = 1;
+//     const res = await fetch(`${BASE_URL.coingecko}/coins/markets?vs_currency=${currency}&per_page=${perPage}&page=${nowPage}`, fetchOptions);
+//     return await res.json();
+// }
+
 /**
  * 코인 리스트
  */
-export async function fetchCoinList2() {
-    const currency = 'usd';
-    const perPage = 20;
-    const nowPage = 1;
-    const res = await fetch(`${BASE_URL.coingecko}/coins/markets?vs_currency=${currency}&per_page=${perPage}&page=${nowPage}`, options);
-    return await res.json();
-}
-
-export async function fetchCoinList(): Promise<ICoins[]> {
+export async function fetchCoinList(): Promise<ICoins[] | any> {
     const currency = 'usd';
     const perPage = 20;
     const nowPage = 1;
 
-    const sessionCoinList = APIS.getSessionStorage('coins:list');
-    if (sessionCoinList) {
-        return new Promise((resolve) => {
-            resolve(sessionCoinList);
-        });
+    const storageKey = 'coins:list';
+    const storageItem = APIS.getSessionStorage(storageKey);
+    if (storageItem) {
+        // return Promise.resolve(sessionCoinList);
+        return storageItem;
     } else {
-        const res = await fetch(`${BASE_URL.coingecko}/coins/markets?vs_currency=${currency}&per_page=${perPage}&page=${nowPage}`, options);
-        const retJson = await res.json();
-        APIS.setSessionStorage('coins:list', retJson);
-        return retJson;
+        const res = await fetch(`${BASE_URL.coingecko}/coins/markets?vs_currency=${currency}&per_page=${perPage}&page=${nowPage}`, fetchOptions);
+        const resJson = await res.json();
+        APIS.setSessionStorage(storageKey, resJson);
+        return APIS.getSessionStorage(storageKey);
     }
 }
 
@@ -50,10 +53,18 @@ export async function fetchCoinList(): Promise<ICoins[]> {
  * 코인 상세정보
  * @param coinId string
  */
-export async function fetchCoinInfo(coinId: string) {
+export async function fetchCoinInfo(coinId: string): Promise<ICoin | any> {
     // https://api.coingecko.com/api/v3/coins/bitcoin?localization=false&tickers=true&market_data=false&community_data=false&developer_data=false&sparkline=false
-    const res = await fetch(`${BASE_URL.coingecko}/coins/${coinId}`, options);
-    return await res.json();
+    const storageKey = `coin:detail`;
+    const storageItem = APIS.getStorageInnerItemWithExpire(storageKey, coinId);
+    if (storageItem) {
+        return storageItem;
+    } else {
+        const res = await fetch(`${BASE_URL.coingecko}/coins/${coinId}`, fetchOptions);
+        const resJson = await res.json();
+        APIS.setStorageInnerItemWithExpire(storageKey, coinId, resJson);
+        return APIS.getStorageInnerItemWithExpire(storageKey, coinId);
+    }
 }
 
 /**
@@ -62,6 +73,8 @@ export async function fetchCoinInfo(coinId: string) {
  * @param coinId string
  */
 export async function fetchCoinTickers(coinId: string) {
+    const storageKey = `coin:${coinId}:tickers`;
+    const storageItem = APIS.getSessionStorage(storageKey);
     const res = await fetch(`${BASE_URL.coingecko}/tickers/${coinId}`);
     return await res.json();
 }
